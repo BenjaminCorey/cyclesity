49122e4e040273e7c2cbd75ec7e72f81ad8433f8
Benjamin Corey Wilkins <becowilk@gmail.com>
2014-03-23 16:54:43 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cyclecity::Application.config.secret_key_base = 'c223b8a34b40fbd5b171ea2e76912d58547852c9859ec6986356d41daa8a7a281d1ed758a8d9bb6f82dec039ecfd6264c8259641cadc9d1563aad41b047fadf2'
